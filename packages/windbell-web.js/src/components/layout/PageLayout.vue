760671d585b9cf36ba4c46e3a70fe727df75f4b6
Xie Yuheng <xyheme@gmail.com>
2026-09-26 23:59:48 +0800

    up

--- packages/windbell-web.js/src/components/layout/PageLayout.vue
+++ packages/windbell-web.js/src/components/layout/PageLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="mx-auto flex w-full max-w-4xl flex-1 flex-col gap-6 px-5 py-6">
+  <main class="mx-auto flex w-full max-w-4xl flex-1 flex-col gap-4 px-4 py-4">
     <slot />
   </main>
 </template>
